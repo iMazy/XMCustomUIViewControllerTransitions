7f90f9d684e3b5b6aa0f4f9a451c21b4bb2b2a54
Mazy <twt_ios@163.com>
2017-01-20 16:53:13 +0800

    添加模态消失功能

---
+++ XMCustomUIViewControllerTransitions/XMCustomUIViewControllerTransitions/Classes/AnimateTranstion/SwipeInteractionController.h
@@ -0,0 +1,13 @@
+//
+//  SwipeInteractionController.h
+//  XMCustomUIViewControllerTransitions
+//
+//  Created by TwtMac on 17/1/20.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwipeInteractionController : UIPercentDrivenInteractiveTransition
+
+@end
---
+++ XMCustomUIViewControllerTransitions/XMCustomUIViewControllerTransitions/Classes/AnimateTranstion/SwipeInteractionController.m
@@ -0,0 +1,13 @@
+//
+//  SwipeInteractionController.m
+//  XMCustomUIViewControllerTransitions
+//
+//  Created by TwtMac on 17/1/20.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+#import "SwipeInteractionController.h"
+
+@implementation SwipeInteractionController
+
+@end
