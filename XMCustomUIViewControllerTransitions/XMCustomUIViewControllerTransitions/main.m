6c346604a6824d7ca88816c01417bb5bae7ede9c
Mazy <twt_ios@163.com>
2017-01-20 14:53:36 +0800

    初始化项目

---
+++ XMCustomUIViewControllerTransitions/XMCustomUIViewControllerTransitions/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMCustomUIViewControllerTransitions
+//
+//  Created by TwtMac on 17/1/20.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
